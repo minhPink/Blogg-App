7a45067a70f37562f085274f6e434f907bafa72c
unknown <kirito14042003@gmail.com>
2024-12-07 10:14:37 +0700

    update header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,9 +6,9 @@ import { FaMoon } from "react-icons/fa";
 
 
 export default function Header() {
-    const path = useLocation().pathname;
+  const path = useLocation().pathname;
   return (
-    <Navbar className='border-b-2'>
+    <Navbar className='border-b-2 px-10 sm:px-10 md:px-10 lg:px-10'>
       <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
       <span className="px-2 py-1 bg-gradient-to-r from-indigo-400 via-purple-400 to-pink-400 rounded-lg text-white">Minh2K3's</span>
       Blog
@@ -29,22 +29,22 @@ export default function Header() {
           <FaMoon/>
         </Button>
         <Link to='/sign-in'>
-          <Button gradientDuoTone="purpleToBlue">
+          <Button gradientDuoTone="purpleToBlue" outline>                  
             Sign In
           </Button>
         </Link>
-        <Navbar.Toggle/>
+        <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-          <Navbar.Link active={path === '/'} as={'div'}>
-            <Link to='/'>Home</Link>
-          </Navbar.Link>
-          <Navbar.Link active={path === '/about'} as={'div'}>
-            <Link to='/about'>About</Link>
-          </Navbar.Link>
-          <Navbar.Link active={path === '/projects'} as={'div'}>
-            <Link to='/projects'>Projects</Link>
-          </Navbar.Link>
+        <Navbar.Link to='/' as={Link} active={path === '/'}>
+          Trang chủ
+        </Navbar.Link>
+        <Navbar.Link to='/about' as={Link} active={path === '/about'}>
+          Giới thiệu
+        </Navbar.Link>
+        <Navbar.Link to='/projects' as={Link} active={path === '/projects'}>
+          Sản phẩm
+        </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
   )
