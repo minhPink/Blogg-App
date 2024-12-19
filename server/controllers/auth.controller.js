02aad9954f72a714b8c9ccc9d1b8010974d82761
Dao Xuan Minh <kirito14042003@gmail.com>
2024-12-19 23:46:54 +0700

    create route signin

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,26 +1,63 @@
 const User = require("../models/user.model");
 
 const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const { errHandler } = require("../utils/error");
 module.exports.signup = async (req, res, next) => {
-    const { username, email, password } = req.body;
+  const { username, email, password } = req.body;
 
-    if (!username || !email || !password || username === '' || email === '' || password === '') {
-        next(errHandler(400, 'All fields are required'));
-    };
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    next(errHandler(400, "All fields are required"));
+  }
 
-    const hashedPassword = bcryptjs.hashSync(password, 10);
+  const hashedPassword = bcryptjs.hashSync(password, 10);
 
-    const newUser = new User({
-        username: username,
-        email: email,
-        password: hashedPassword
-    });
+  const newUser = new User({
+    username: username,
+    email: email,
+    password: hashedPassword,
+  });
 
-    try {
-        await newUser.save();
-        res.json({ message: 'Signup successful'});
-    } catch (error) {
-        next(error);
+  try {
+    await newUser.save();
+    res.json({ message: "Signup successful" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    return next(errHandler(400, "All fields are reuired"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email: email });
+    if (!validUser) {
+      return next(errHandler(404, "User not found"));
     }
-}
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errHandler(400, "Invalid password"));
+    }
+    const token = jwt.sign({ id: validUser }, process.env.JWT_SECRET);
+
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .status(200)
+      .cookie("access_token", token, { httpOnly: true })
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const controller = require("../controllers/auth.controller.js");
 
 router.post("/signup", controller.signup);
+router.post("/signin", controller.signin);
 
 module.exports = router;
