7f32cd480789dfada6103fd2c8bb15df676ba519
Dao Xuan Minh <kirito14042003@gmail.com>
2025-02-09 16:24:09 +0700

    update

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,19 +1,45 @@
 import { Button, TextInput } from "flowbite-react";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const filePickerRef = useRef();
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(e.target.files[0]);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  };
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage();
+    }
+  }, [imageFile]);
+  const uploadImage = async () => {
+    console.log("uploading image...");
+  };
   return (
     <div className="max-w-lg w-full mx-auto p-3">
       <h1 className="my-7 text-center font-semibold text-3xl">
         Thông tin cá nhân
       </h1>
       <form className="flex flex-col gap-4">
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          ref={filePickerRef}
+          hidden
+        />
         <img
-          src={currentUser.profilePicture}
+          src={imageFileUrl || currentUser.profilePicture}
           alt="user"
           className="rounded-full object-cover h-32 w-32 mx-auto"
+          onClick={() => filePickerRef.current.click()}
         />
         <TextInput
           type="text"
